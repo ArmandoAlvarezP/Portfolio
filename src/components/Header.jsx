39bb08be88355f4af0764a33b9f9c3d0afcf41e5
AalvarezP <armando.aperez.gk@gmail.com>
2023-07-20 11:28:46 -0600

    Diseño de Header y Footer Terminado v_1.2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,19 +4,21 @@ export const Header = () => {
   return (
     <header className="p-10">
       <nav className="container mx-auto flex flex-col sm:flex-row justify-between text-center items-center gap-5">
-        <h1 className="text-white font-bold uppercase">Portfolio Armando Alvarez</h1>
-        <Link to='/projects' className="text-white font-bold uppercase md:flex-row">
-          Projects
-        </Link>
-        <Link to='/about' className="text-white font-bold uppercase">
-          About Me
-        </Link>
-        <Link to='/contact' className="text-white font-bold uppercase">
-          Contact Me
-        </Link>
-        <Link to='/resume' className="text-white font-bold uppercase">
-          Resume
-        </Link>
+        <h1 className="text-white font-bold uppercase">Armando Alvarez</h1>
+        <div className="flex flex-col justify-end md:flex-row">
+            <Link to='/projects' className="text-white font-bold uppercase mx-5">
+            Projects
+            </Link>
+            <Link to='/about' className="text-white font-bold uppercase mx-5">
+            About Me
+            </Link>
+            <Link to='/contact' className="text-white font-bold uppercase mx-5">
+            Contact Me
+            </Link>
+            <Link to='/resume' className="text-white font-bold uppercase mx-5">
+            Resume
+            </Link>
+        </div>
       </nav>
     </header>
   )
