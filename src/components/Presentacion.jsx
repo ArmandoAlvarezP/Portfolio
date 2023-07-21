31903019cd6152487d0ebe23446cdc6dfddd43c4
AalvarezP <armando.aperez.gk@gmail.com>
2023-07-21 13:09:57 -0600

    Ajuste de pocisión de  texto de presentación

--- src/components/Presentacion.jsx
+++ src/components/Presentacion.jsx
@@ -1,7 +1,7 @@
 
 export const Presentacion = () => {
   return (
-    <div className="ms-5 md:ms-0">
+    <div className="ms-0 md:ms-10">
       <div className="typing">
         <h1>!Hola, soy Armando!...</h1>
       </div>
